ef4fc001c52616287293487ee5ee218fbb49d8a0
SeniorJava <smartandroid.com@gmail.com>
2015-06-11 13:02:04 +0300

    patch1.3(modiff1)

---
+++ src/main/java/ru/javawebinar/topjava/web/UserServlet.java
@@ -0,0 +1,16 @@
+package ru.javawebinar.topjava.web;
+
+import java.io.IOException;
+
+/**
+ * Created by senior on 11.06.15.
+ */
+public class UserServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}
