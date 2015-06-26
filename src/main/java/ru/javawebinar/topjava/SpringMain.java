bb62cc0f4777d4314aeabe29a1342cca86dfaacb
SeniorJava <smartandroid.com@gmail.com>
2015-06-26 17:24:18 +0300

    5 patch

---
+++ src/main/java/ru/javawebinar/topjava/SpringMain.java
@@ -0,0 +1,23 @@
+package ru.javawebinar.topjava;
+
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import ru.javawebinar.topjava.model.Role;
+import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.service.UserService;
+
+import java.util.Arrays;
+
+/**
+ * User: gkislin
+ * Date: 22.08.2014
+ */
+public class SpringMain {
+    public static void main(String[] args) {
+        ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml");
+        System.out.println(Arrays.toString(appCtx.getBeanDefinitionNames()));
+        UserService userService = appCtx.getBean(UserService.class);
+        System.out.println(userService.save(new User(1, "userName", "email", "password", Role.ROLE_ADMIN)));
+        appCtx.close();
+    }
+}
