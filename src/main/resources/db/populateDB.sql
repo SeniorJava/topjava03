cb30e930071e3fd14f5c56f95c267c29ebc0431c
SeniorJava <smartandroid.com@gmail.com>
2015-06-28 20:55:51 +0300

    Дополнил скрипты создания и инициализации базы

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,5 +1,6 @@
 DELETE FROM user_roles;
 DELETE FROM users;
+DELETE FROM meals;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 -- password
@@ -11,3 +12,7 @@ VALUES ('Admin', 'admin@gmail.com', 'admin');
 
 INSERT INTO user_roles (role, user_id) VALUES ('ROLE_USER', 100000);
 INSERT INTO user_roles (role, user_id) VALUES ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals (user_id, time, calories, description) VALUES (100000,'1994-11-27', 150, 'бутербродик с черной икрой');
+INSERT INTO meals (user_id, time, calories, description) VALUES (100000,'1994-11-28', 300, 'лапша мивина');
+INSERT INTO meals (user_id, time, calories, description) VALUES (100001,'1994-11-28', 0, 'что такое еда?');
