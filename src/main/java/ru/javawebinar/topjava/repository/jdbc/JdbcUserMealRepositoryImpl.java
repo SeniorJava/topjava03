4110799574d7b409891cf59988aacdc42db2577f
SeniorJava <smartandroid.com@gmail.com>
2015-06-29 12:31:54 +0300

    поигрался с JdbcUserMealRepositoryImpl

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcUserMealRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcUserMealRepositoryImpl.java
@@ -1,9 +1,16 @@
 package ru.javawebinar.topjava.repository.jdbc;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
+import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.model.UserMeal;
 import ru.javawebinar.topjava.repository.UserMealRepository;
 
+import javax.sql.DataSource;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -15,19 +22,34 @@ import java.util.List;
 @Repository
 public class JdbcUserMealRepositoryImpl implements UserMealRepository {
 
+    private static final BeanPropertyRowMapper<UserMeal> ROW_MAPPER = BeanPropertyRowMapper.newInstance(UserMeal.class);
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    private SimpleJdbcInsert insertMeal;
+
+    public JdbcUserMealRepositoryImpl(DataSource dataSource) {
+        this.insertMeal = new SimpleJdbcInsert(dataSource)
+        .withTableName("MEALS").usingGeneratedKeyColumns("id");
+    }
+
     @Override
-    public UserMeal save(UserMeal UserMeal, int userId) {
-        return null;
+    public UserMeal save(UserMeal userMeal, int userId) {
+        MapSqlParameterSource map = new MapSqlParameterSource()
+                .addValue("time",userMeal.getDateTime())
+                .addValue("calories",userMeal.getCalories())
+                .addValue("description",userMeal.getDescription());
+        return userMeal;
     }
 
     @Override
     public boolean delete(int id, int userId) {
-        return false;
+        return jdbcTemplate.update("DELETE FROM meals WHERE meal_id=?",id) !=0;
     }
 
     @Override
     public UserMeal get(int id, int userId) {
-        return null;
+        return jdbcTemplate.queryForObject("SELECT * FROM meals WHERE meal_id=?",ROW_MAPPER, id);
     }
 
     @Override
