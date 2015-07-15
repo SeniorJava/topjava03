4c43d007bbd20d3e2d6aaf18107a2529b1e13819
SeniorJava <smartandroid.com@gmail.com>
2015-07-15 15:34:32 +0300

    create LocalDateTimeConverter

---
+++ src/main/java/ru/javawebinar/topjava/model/LocalDateTimeConverter.java
@@ -0,0 +1,25 @@
+package ru.javawebinar.topjava.model;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+/**
+ * Created by senior on 15.07.15.
+ */
+@Converter(autoApply = true)
+public class LocalDateTimeConverter implements AttributeConverter<LocalDateTime, Timestamp> {
+
+    @Override
+    public java.sql.Timestamp convertToDatabaseColumn(LocalDateTime entityValue) {
+        return Timestamp.valueOf(entityValue);
+    }
+
+    @Override
+    public LocalDateTime convertToEntityAttribute(java.sql.Timestamp databaseValue) {
+        return databaseValue.toLocalDateTime();
+    }
+
+
+}
